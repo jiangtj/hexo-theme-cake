57bf5a5328fb255bb4a861c292eb8d8f87e3a192
jiangtj <116749895@qq.com>
2019-06-21 17:06:20 +0800

    ...

--- scripts/tags/note.js
+++ scripts/tags/note.js
@@ -17,9 +17,34 @@
 'use strict';
 
 function postNote(args, content) {
-  return `<div class="note ${args.join(' ')}">
-            ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
-          </div>`;
+
+  let style = 'default';
+  let title = '';
+  let icon = true;
+
+  if (Array.isArray(args)) {
+
+    let iconIndex = args.indexOf('icon:disable');
+    if (iconIndex > -1) {
+      icon = false;
+      args.splice(iconIndex, 1);
+    }
+
+    if (args.length > 0) {
+      style = args.shift();
+    }
+
+    if (!icon) {
+      style += ' no-icon';
+    }
+
+    if (args.length > 0) {
+      title = "<header>" + args.join(" ") + "</header>";
+    }
+
+  }
+
+  return `<div class="note ${style}">${title}${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}</div>`;
 }
 
 hexo.extend.tag.register('note', postNote, {ends: true});
