0250c0e27a79c90d745e379f1e57df81f04cd34d
jiangtj <116749895@qq.com>
2019-07-15 16:40:50 +0800

    Use merge

--- scripts/config.js
+++ scripts/config.js
@@ -1,14 +1,16 @@
 'use strict';
 
+const merge = require('lodash.merge');
+
 module.exports = function(hexo) {
 
   let data = hexo.locals.get('data');
 
   // Read config from data.next or theme_config
   if (data && data.next) {
-    Object.assign(hexo.theme.config, data.next);
+    merge(hexo.theme.config, data.next);
   } else {
-    Object.assign(hexo.theme.config, hexo.config.theme_config);
+    merge(hexo.theme.config, hexo.config.theme_config);
   }
 
   // Custom languages support.
@@ -16,7 +18,7 @@ module.exports = function(hexo) {
     var lang = hexo.config.language;
     var i18n = hexo.theme.i18n;
     var mergeLang = function (lang) {
-      i18n.set(lang, Object.assign(i18n.get([lang]), data.languages[lang]));
+      i18n.set(lang, merge(i18n.get([lang]), data.languages[lang]));
     };
     if (Array.isArray(lang)) {
       for (var i = 0; i < lang.length; i++) {
