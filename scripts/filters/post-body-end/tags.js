19223225d9d7a85916fa694b11e0dc6b32e492e0
MrTT <jiang.taojie@foxmail.com>
2019-07-19 23:02:20 +0800

    Re tags

---
+++ scripts/filters/post-body-end/tags.js
@@ -0,0 +1,17 @@
+/* global hexo */
+
+'use strict';
+
+const priority = hexo.config.inject_priority_tags || 300;
+
+hexo.extend.filter.register('theme_inject', function(injects) {
+  injects.postBodyEnd.raw('tags', `
+    {% if page.tags and page.tags.length %}
+      <div class="post-tags">
+        {% for tag in page.tags %}
+          <a href="{{ url_for(tag.path) }}" rel="tag"># {{ tag.name }}</a>
+        {% endfor %}
+      </div>
+    {% endif %}
+  `);
+}, priority);
