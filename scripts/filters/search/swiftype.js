217a64b13df6bcebc788bf553d35f3c03b0020b8
Mr.J <116749895@qq.com>
2019-07-25 14:56:01 +0800

    Refactor search (#11)
    * Refactor
    
    * Fix loacl search
    
    * Rm local search
    
    * Rm local search config

---
+++ scripts/filters/search/swiftype.js
@@ -0,0 +1,25 @@
+/* global hexo */
+
+'use strict';
+
+const path = require('path');
+
+// Add comment
+hexo.extend.filter.register('theme_inject', function(injects) {
+  let theme = hexo.theme.config;
+  if (!theme.swiftype_key) return;
+
+  injects.menu.raw('swiftype', `
+  <li class="menu-item menu-item-search">
+    <a href="javascript:;" class="st-search-show-outputs">
+      {% if theme.menu_settings.icons %}
+        <i class="menu-item-icon fa fa-search fa-fw"></i> <br/>
+      {% endif %}
+      {{ __('menu.search') }}
+    </a>
+  </li>
+  `, {}, {cache: true});
+
+  injects.bodyEnd.file('swiftype', path.join(hexo.theme_dir, 'layout/_third-party/search/swiftype.swig'));
+
+});
--- scripts/injects-point.js
+++ scripts/injects-point.js
@@ -1,6 +1,21 @@
 'use strict';
 
 module.exports = {
-  views: ['head', 'header', 'sidebar', 'postMeta', 'postBodyEnd', 'footer', 'bodyEnd', 'reward', 'comment'],
-  styles: ['variable', 'mixin', 'style']
+  views: [
+    'head',
+    'header',
+    'sidebar',
+    'postMeta',
+    'postBodyEnd',
+    'footer',
+    'bodyEnd',
+    'reward',
+    'comment',
+    'menu'
+  ],
+  styles: [
+    'variable', 
+    'mixin', 
+    'style'
+  ]
 };
