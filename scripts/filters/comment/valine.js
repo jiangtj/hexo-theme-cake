32976f81636e6f8892da6d21fd29dd2f2e5d082d
jiangtj <116749895@qq.com>
2019-07-25 10:24:48 +0800

    Add livere

--- scripts/filters/comment/valine.js
+++ scripts/filters/comment/valine.js
@@ -9,9 +9,9 @@ const {iconText} = require('./common');
 // Add comment
 hexo.extend.filter.register('theme_inject', function(injects) {
   let theme = hexo.theme.config;
-  if (!theme.valine.enable) return;
-  
-  injects.comment.raw('valine', `<div class="comments" id="comments"></div>`, {}, {cache: true});
+  if (!theme.valine.enable || !theme.valine.appid || !theme.valine.appkey) return;
+
+  injects.comment.raw('valine', '<div class="comments" id="comments"></div>', {}, {cache: true});
 
   injects.bodyEnd.file('valine', path.join(hexo.theme_dir, 'layout/_third-party/comments/valine.swig'));
 
@@ -20,10 +20,10 @@ hexo.extend.filter.register('theme_inject', function(injects) {
 // Add post_meta
 hexo.extend.filter.register('theme_inject', function(injects) {
   let theme = hexo.theme.config;
-  if (!theme.valine.enable) return;
-  
+  if (!theme.valine.enable || !theme.valine.appid || !theme.valine.appkey) return;
+
   injects.postMeta.raw('valine', `
-  {% if post.comments %}
+  {% if post.comments and (is_post() or theme.valine.comment_count) %}
   <span class="post-meta-item">
     ${iconText}
     <a href="{{ url_for(post.path) }}#comments" itemprop="discussionUrl">
