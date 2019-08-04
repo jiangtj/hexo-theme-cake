bdb272d23b5db0cb90c95f3759392115b0e2da79
MrTT <jiang.taojie@foxmail.com>
2019-08-04 16:57:48 +0800

    Add button style support

--- scripts/filters/comment/default-config.js
+++ scripts/filters/comment/default-config.js
@@ -4,12 +4,21 @@
 
 const path = require('path');
 
-hexo.extend.filter.register('theme_inject', function(injects) {
+hexo.extend.filter.register('theme_inject', injects => {
   injects.comment.raws.forEach(element => {
+    // Set default button content
     let injectName = path.basename(element.name, path.extname(element.name));
     element.args[0] = Object.assign({
       class: injectName,
       button: injectName
     }, element.args[0]);
+    // Set custom button content
+    let locals = element.args[0];
+    if (hexo.theme.config.comments.button) {
+      let customButton = hexo.theme.config.comments.button[locals.class];
+      if (customButton) {
+        locals.button = customButton;
+      }
+    }
   });
 }, 99999);
