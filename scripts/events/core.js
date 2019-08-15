ec7cc2a8c6179612a802404f0126f9bf4197ef4d
Mr.J <116749895@qq.com>
2019-08-15 19:36:07 +0800

    fix-categories-tags (#13)

--- scripts/events/core.js
+++ scripts/events/core.js
@@ -7,4 +7,6 @@ hexo.on('generateBefore', function () {
   require('./lib/config')(hexo);
   // Add filter type `theme_inject`.
   require('./lib/injects')(hexo);
+  // Fix categories and tags.
+  require('./lib/fix-categories-tags')(hexo);
 });
