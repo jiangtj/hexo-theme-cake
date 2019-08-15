e9ddfcfbc126d7f07a1130063d895f3b2fd5b9b1
jiangtj <116749895@qq.com>
2019-08-15 17:54:05 +0800

    fix-categories-tags

---
+++ scripts/events/lib/fix-categories-tags.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function (hexo) {
+  hexo.locals.set('valid_categories', function () {
+    return hexo.locals.get('categories')
+      .filter(category => category.length !== 0);
+  });
+  hexo.locals.set('valid_tags', function () {
+    return hexo.locals.get('tags')
+      .filter(tag => tag.length !== 0);
+  });
+};
