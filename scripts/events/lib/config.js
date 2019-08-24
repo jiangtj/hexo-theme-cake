9b73309f083037b5e8a145416e57cd7896041c17
jiangtj <116749895@qq.com>
2019-08-24 13:42:16 +0800

    Refactor config

--- scripts/events/lib/config.js
+++ scripts/events/lib/config.js
@@ -4,26 +4,20 @@ const { merge } = require('lodash');
 
 module.exports = function(hexo) {
 
-  let data = hexo.locals.get('data');
+  let data = hexo.locals.get('data') || {};
 
   // Read config from data.next or theme_config
-  if (data && data.next) {
-    merge(hexo.theme.config, data.next);
-  } else {
-    merge(hexo.theme.config, hexo.config.theme_config);
-  }
+  merge(hexo.theme.config, hexo.config.theme_config, data.next);
 
   // Custom languages support.
-  if (data && data.languages) {
-    var lang = hexo.config.language;
-    var i18n = hexo.theme.i18n;
-    var mergeLang = function(lang) {
+  if (data.languages) {
+    let lang = hexo.config.language;
+    let i18n = hexo.theme.i18n;
+    let mergeLang = function(lang) {
       i18n.set(lang, merge(i18n.get([lang]), data.languages[lang]));
     };
     if (Array.isArray(lang)) {
-      for (var i = 0; i < lang.length; i++) {
-        mergeLang(lang[i]);
-      }
+      lang.forEach(item => mergeLang(item));
     } else {
       mergeLang(lang);
     }
