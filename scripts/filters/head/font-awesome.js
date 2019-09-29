f69bf589d2ab7b5f9962b01b47092356d9b2b2b1
Mr.J <116749895@qq.com>
2019-09-29 16:12:08 +0800

    Upgrade font-awesome 4 to 5 (#22)
    * Upgrade font-awesome 4 to 5
    
    * Rm post-edit.swig
    
    * Rm version
    
    * Rm breadcrumb

---
+++ scripts/filters/head/font-awesome.js
@@ -0,0 +1,18 @@
+/* global hexo */
+
+'use strict';
+
+hexo.extend.filter.register('theme_inject', function(injects) {
+  let config = hexo.theme.config.fontawesome;
+  if (config.type === 'cdn') {
+    let integrity = config.integrity;
+    if (integrity) {
+      integrity = `integrity="${integrity}"`;
+    }
+    injects.head.raw('font-awesome', `<link rel="stylesheet" href="${config.url}" ${integrity} crossorigin="anonymous">`, {}, {only: true, cache: true});
+    return;
+  }
+  if (config.type === 'kit') {
+    injects.head.raw('font-awesome', `<script src="${config.url}" crossorigin="anonymous"></script>`, {}, {only: true, cache: true});
+  }
+});
