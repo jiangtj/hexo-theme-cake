9f0ae5e92f5e2e299d065e4f1faad5dd816da68b
jiangtj <116749895@qq.com>
2019-10-18 13:55:10 +0800

    Update locals

--- scripts/helpers/next-js.js
+++ scripts/helpers/next-js.js
@@ -3,6 +3,6 @@
 'use strict';
 
 hexo.extend.helper.register('next_js', function(...urls) {
-  let version = this.next_env('version');
+  let version = hexo.locals.get('cake').theme.version;
   return urls.map((url) => this.js(`js/${url}?v=${version}`)).join('');
 });
