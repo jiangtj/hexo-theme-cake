0650f52a0153ceda9393c6cc752adc1983f0695e
jiangtj <116749895@qq.com>
2019-10-21 17:19:50 +0800

    Use Object.assign

--- scripts/events/lib/config.js
+++ scripts/events/lib/config.js
@@ -1,20 +1,18 @@
 'use strict';
 
-const { merge } = require('lodash');
-
 module.exports = function(hexo) {
 
   let data = hexo.locals.get('data') || {};
 
   // Read config from data.next or theme_config
-  merge(hexo.theme.config, hexo.config.theme_config, data.next);
+  Object.assign(hexo.theme.config, hexo.config.theme_config, data.next);
 
   // Custom languages support.
   if (data.languages) {
     let lang = hexo.config.language;
     let i18n = hexo.theme.i18n;
     let mergeLang = function(lang) {
-      i18n.set(lang, merge(i18n.get([lang]), data.languages[lang]));
+      i18n.set(lang, Object.assign(i18n.get([lang]), data.languages[lang]));
     };
     if (Array.isArray(lang)) {
       lang.forEach(item => mergeLang(item));
