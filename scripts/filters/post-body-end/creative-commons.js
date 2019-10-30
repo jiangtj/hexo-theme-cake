488e3a21f9dba019fbbfd987045b3b09fd19c787
jiangtj <116749895@qq.com>
2019-10-30 12:42:27 +0800

    copu

--- scripts/filters/post-body-end/creative-commons.js
+++ scripts/filters/post-body-end/creative-commons.js
@@ -7,6 +7,6 @@ const path = require('path');
 hexo.extend.filter.register('theme_inject', function(injects) {
   let theme = hexo.theme.config;
   if (theme.creative_commons.license && theme.creative_commons.post) {
-    injects.postBodyEnd.file('creative-commons', path.join(hexo.theme_dir, 'layout/_partials/post/post-copyright.swig'), {}, {}, theme.creative_commons.inject_order);
+    injects.postBodyEnd.file('creative-commons', path.join(hexo.theme_dir, 'layout/_partials/post/post-copyright.ejs'), {}, {}, theme.creative_commons.inject_order);
   }
 }, 200);
