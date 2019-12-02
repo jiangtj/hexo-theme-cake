16a7485d4b95ea49b11f0f0c66221df72e83d0ce
Mr.J <116749895@qq.com>
2019-12-02 18:12:30 +0800

    Refactor head: use helper (#30)

---
+++ scripts/helpers/cake-title.js
@@ -0,0 +1,33 @@
+/* global hexo */
+
+'use strict';
+
+hexo.extend.helper.register('cake_title', function() {
+  let {__, is_home, is_post, is_page, is_archive, is_category, is_tag} = this;
+  let {theme, page, subtitle, title} = this;
+  let cakeTitle = '';
+  if (is_home()) {
+    cakeTitle = title;
+    if (theme.index_with_subtitle && subtitle) {
+      cakeTitle += ` - ${subtitle}`;
+    }
+  } else if (is_post()) {
+    cakeTitle = `${page.title} | ${title}`;
+  } else if (is_page()) {
+    let page_title_suffix = ' | ' + title;
+    if (page.type === 'categories' && !page.title) {
+      cakeTitle = __('title.category') + page_title_suffix;
+    } else if (page.type === 'tags' && !page.title) {
+      cakeTitle = __('title.tag') + page_title_suffix;
+    } else {
+      cakeTitle = page.title + page_title_suffix;
+    }
+  } else if (is_archive()) {
+    cakeTitle = `${__('title.archive')} | ${title}`;
+  } else if (is_category()) {
+    cakeTitle = `${__('title.category')}:${page.category} | ${title}`;
+  } else if (is_tag()) {
+    cakeTitle = `${__('title.tag')}:${page.tag} | ${title}`;
+  }
+  return `<title>${cakeTitle}</title>`;
+});
