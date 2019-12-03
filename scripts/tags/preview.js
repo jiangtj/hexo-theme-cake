f7c25f801c6c9b0691be2f60141addbd92be0a64
Mr.J <116749895@qq.com>
2019-12-03 20:46:22 +0800

    Add preview tag (#31)

---
+++ scripts/tags/preview.js
@@ -0,0 +1,46 @@
+/* global hexo */
+
+/**
+ * {% preview %}
+ * <template>
+ *   html code
+ * </template>
+ * <style>
+ *   css code
+ * </style>
+ * <script>
+ *   js code
+ * </script>
+ * {% endpreview %}
+ */
+
+'use strict';
+
+function getContent(content, tag) {
+  let index = content.indexOf(`<${tag}>`);
+  if (index === -1) {
+    return '';
+  }
+  let endIndex = content.indexOf(`</${tag}>`);
+  return content.substring(index + tag.length + 2, endIndex);
+}
+
+function preview(args, content) {
+  let js = getContent(content, 'script');
+  let css = getContent(content, 'style');
+  let html = getContent(content, 'template');
+
+  let out = '';
+  if (html) {
+    out += '<header style="margin-bottom: -20px">html</header>' + hexo.render.renderSync({text: `\`\`\`html${html}\`\`\``, engine: 'markdown'});
+  }
+  if (js) {
+    out += '<header style="margin-bottom: -20px">js</header>' + hexo.render.renderSync({text: `\`\`\`js${js}\`\`\``, engine: 'markdown'});
+  }
+  if (css) {
+    out += '<header style="margin-bottom: -20px">css</header>' + hexo.render.renderSync({text: `\`\`\`js${css}\`\`\``, engine: 'markdown'});
+  }
+  return `${out}<style>${css}</style><header style="margin-bottom: -20px">result</header><div class="template">${html}</div><script>${js}</script>`;
+}
+
+hexo.extend.tag.register('preview', preview, {ends: true});
