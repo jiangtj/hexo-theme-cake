8ff3af40a18c11a63ce5c5d36a4459dee8b457fc
jiangtj <116749895@qq.com>
2019-12-24 09:45:40 +0800

    Fix lozad

--- scripts/filters/lozad.js
+++ scripts/filters/lozad.js
@@ -7,7 +7,9 @@ const {encodeURL} = require('hexo-util');
 hexo.extend.filter.register('marked:renderer', function(renderer) {
   if (!hexo.theme.config.lozad.enable) return;
   renderer.image = function(href, title, text) {
-    return `<img data-src="${encodeURL(href)}" alt="${text}">`;
+    let titleAttr = title ? ` title="${title}"` : '';
+    let textAttr = text ? ` alt="${text}"` : '';
+    return `<img data-src="${encodeURL(href)}"${titleAttr}${textAttr}>`;
   };
 }, 99);
 
