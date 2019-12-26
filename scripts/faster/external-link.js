ea342ad8602df46fec61f765b9a8af635b4783b6
jiangtj <116749895@qq.com>
2019-12-26 11:07:48 +0800

    faster

--- scripts/faster/external-link.js
+++ scripts/faster/external-link.js
@@ -1,6 +1,5 @@
 /* global hexo */
 
-const {isExternalLink} = require('hexo-util');
 const {filter} = hexo.extend;
 
 const afterPostRenders = filter.list().after_post_render;
@@ -15,37 +14,3 @@ for (let fn of afterRenders) {
     filter.unregister('after_render:html', fn);
   }
 }
-
-const external_link = Object.assign({
-  enable : true,
-  field  : 'site',
-  exclude: []
-}, hexo.config.external_link);
-
-const exclude = Array.isArray(external_link.exclude) ? external_link.exclude
-  : [external_link.exclude];
-
-function getExternalLink(href) {
-  if (external_link) {
-    let target = ' target="_blank"';
-    let noopener = ' rel="noopener"';
-    let nofollowTag = ' rel="noopener external nofollow noreferrer"';
-    if (isExternalLink(href, hexo.config.url, exclude)) {
-      if (external_link.enable && external_link.nofollow) {
-        return target + nofollowTag;
-      } else if (external_link.enable) {
-        return target + noopener;
-      } else if (external_link.nofollow) {
-        return nofollowTag;
-      }
-    }
-  } else {
-    return '';
-  }
-}
-
-filter.register('marked:link', (data, options) => {
-  let { href, text } = data;
-  let extAttr = getExternalLink(href);
-  data.content = data.content.replace(`>${text}</a>`, `${extAttr}>${text}</a>`);
-}, 20);
