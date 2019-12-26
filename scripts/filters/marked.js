ad703e5d6fecf221aad59ff094de12f3b8ec3cb0
Mr.J <116749895@qq.com>
2019-12-26 16:52:44 +0800

    Fix hexo-renderer-marked compatibility (#36)

---
+++ scripts/filters/marked.js
@@ -0,0 +1,19 @@
+/* global hexo */
+
+'use strict';
+
+// If not use this md render, some feature will break.
+if (hexo.config.disable_cake_marked) return;
+
+const { highlight } = require('hexo-util');
+
+hexo.extend.filter.register('marked:renderer', function(renderer) {
+  renderer.code = (code, infostring, escaped) => {
+    return highlight(code, {
+      lang  : infostring,
+      gutter: hexo.config.highlight.line_number,
+      tab   : hexo.config.highlight.tab_replace,
+      wrap  : hexo.config.highlight.wrap
+    });
+  };
+}, 99);
--- scripts/tags/preview.js
+++ scripts/tags/preview.js
@@ -30,17 +30,31 @@ function preview(args, content) {
   let css = getContent(content, 'style');
   let html = getContent(content, 'template');
 
-  let out = '';
-  if (html) {
-    out += '<header style="margin-bottom: -20px">html</header>' + hexo.render.renderSync({text: `\`\`\`html${html}\`\`\``, engine: 'markdown'});
-  }
-  if (js) {
-    out += '<header style="margin-bottom: -20px">js</header>' + hexo.render.renderSync({text: `\`\`\`js${js}\`\`\``, engine: 'markdown'});
-  }
-  if (css) {
-    out += '<header style="margin-bottom: -20px">css</header>' + hexo.render.renderSync({text: `\`\`\`js${css}\`\`\``, engine: 'markdown'});
-  }
-  return `${out}<style>${css}</style><header style="margin-bottom: -20px">result</header><div class="template">${html}</div><script>${js}</script>`;
+  return Promise.all([
+    Promise.resolve(html)
+      .then(html => {
+        if (html) return hexo.render.render({text: `\`\`\`html${html}\`\`\``, engine: 'markdown'});
+      })
+      .then(result => {
+        if (result) return Promise.resolve('<header style="margin-bottom: -20px">html</header>' + result);
+      }),
+    Promise.resolve(js)
+      .then(js => {
+        if (js) return hexo.render.render({text: `\`\`\`js${js}\`\`\``, engine: 'markdown'});
+      })
+      .then(result => {
+        if (result) return Promise.resolve('<header style="margin-bottom: -20px">js</header>' + result);
+      }),
+    Promise.resolve(css)
+      .then(css => {
+        if (css) return hexo.render.render({text: `\`\`\`css${css}\`\`\``, engine: 'markdown'});
+      })
+      .then(result => {
+        if (result) return Promise.resolve('<header style="margin-bottom: -20px">css</header>' + result);
+      })
+  ]).then((values) => {
+    return `${values.join('')}<style>${css}</style><header style="margin-bottom: -20px">result</header><div class="template">${html}</div><script>${js}</script>`;
+  });
 }
 
-hexo.extend.tag.register('preview', preview, {ends: true});
+hexo.extend.tag.register('preview', preview, {ends: true, async: true});
