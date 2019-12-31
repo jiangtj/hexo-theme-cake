e4df5f21e982a8ef67d7b0aacd6ab759793ec121
jiangtj <116749895@qq.com>
2019-12-31 13:01:19 +0800

    Add {% raw %} warpper

--- scripts/filters/marked.js
+++ scripts/filters/marked.js
@@ -9,11 +9,13 @@ const { highlight } = require('hexo-util');
 
 hexo.extend.filter.register('marked:renderer', function(renderer) {
   renderer.code = (code, infostring, escaped) => {
-    return highlight(code, {
-      lang  : infostring,
-      gutter: hexo.config.highlight.line_number,
-      tab   : hexo.config.highlight.tab_replace,
-      wrap  : hexo.config.highlight.wrap
-    });
+    return '{% raw %}'
+      + highlight(code, {
+        lang  : infostring,
+        gutter: hexo.config.highlight.line_number,
+        tab   : hexo.config.highlight.tab_replace,
+        wrap  : hexo.config.highlight.wrap
+      })
+      + '{% endraw %}';
   };
 }, 99);
