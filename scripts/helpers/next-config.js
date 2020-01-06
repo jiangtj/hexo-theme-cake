1a6fbf65965b4439e5fd0669216ee60d06598d89
jiangtj <116749895@qq.com>
2020-01-06 13:40:24 +0800

    Add next_config

---
+++ scripts/helpers/next-config.js
@@ -0,0 +1,24 @@
+/* global hexo */
+
+'use strict';
+
+const {Cache} = require('hexo-util');
+const cache = new Cache();
+
+hexo.extend.helper.register('next_config', function() {
+  return cache.apply('config', () => {
+    let {theme, cake_version} = this;
+    let config = {
+      root    : theme.root,
+      scheme  : theme.scheme,
+      version : cake_version,
+      sidebar : theme.sidebar,
+      comments: theme.comments,
+      scroll  : {
+        save: theme.scroll.save
+      }
+    };
+    return `<script id="hexo.configurations">var NexT = window.NexT || {};var CONFIG = ${JSON.stringify(config)}</script>`;
+  });
+
+});
