8bcdedc2bb04024c6f3268a5f67e94008f0d57be
jiangtj <116749895@qq.com>
2020-01-06 12:25:28 +0800

    Update external_link

--- scripts/filters/marked.js
+++ scripts/filters/marked.js
@@ -7,15 +7,29 @@ if (hexo.config.disable_cake_marked) return;
 
 const { highlight } = require('hexo-util');
 
+// external_link
+if (hexo.config.external_link === true) {
+  hexo.config.external_link = {
+    enable: true
+  };
+}
+hexo.config.marked = Object.assign({
+  external_link: {
+    enable  : hexo.config.external_link.enable,
+    exclude : hexo.config.external_link.exclude,
+    nofollow: false
+  }
+}, hexo.config.marked);
+
+// code
 hexo.extend.filter.register('marked:renderer', function(renderer) {
   renderer.code = (code, infostring, escaped) => {
-    return '{% raw %}'
-      + highlight(code, {
-        lang  : infostring,
-        gutter: hexo.config.highlight.line_number,
-        tab   : hexo.config.highlight.tab_replace,
-        wrap  : hexo.config.highlight.wrap
-      })
-      + '{% endraw %}';
+    let rendered = highlight(code, {
+      lang  : infostring,
+      gutter: hexo.config.highlight.line_number,
+      tab   : hexo.config.highlight.tab_replace,
+      wrap  : hexo.config.highlight.wrap
+    });
+    return `{% raw %}${rendered}{% endraw %}`;
   };
 }, 99);
