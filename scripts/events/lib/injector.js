5b352dc3e07a6151b30af64fa861a0f1e71e42a7
jiangtj <116749895@qq.com>
2020-01-08 16:21:13 +0800

    Add Injector.is helper

--- scripts/events/lib/injector.js
+++ scripts/events/lib/injector.js
@@ -31,10 +31,27 @@ class Injector {
     if (!entry) throw new TypeError('entry is required');
     this.store[entry] = this.store[entry] || [];
 
+    if (typeof predicate === 'string') {
+      predicate = this.is(predicate);
+    }
+
     if (typeof value === 'object') {
       this.store[entry].push(Object.assign({ predicate, priority }, value));
     }
     this.store[entry].push({ value, predicate, priority });
+  }
+
+  is(type) {
+    let predicate = locals => locals.page[type];
+    switch (type) {
+      case 'home': predicate = locals => locals.page.__index; break;
+      case 'post': predicate = locals => locals.page.__post; break;
+      case 'page': predicate = locals => locals.page.__page; break;
+      case 'archive': predicate = locals => locals.page.archive; break;
+      case 'category': predicate = locals => locals.page.category; break;
+      case 'tag': predicate = locals => locals.page.tag; break;
+    }
+    return predicate;
   }
 }
 
