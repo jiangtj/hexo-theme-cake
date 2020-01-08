5d278bc7c22b27b8e514eb784828db9983275178
MrTT <jiang.taojie@foxmail.com>
2020-01-08 21:42:01 +0800

    Update reward

--- scripts/filters/post-body-end/reward.js
+++ scripts/filters/post-body-end/reward.js
@@ -3,19 +3,30 @@
 'use strict';
 
 const path = require('path');
+const {Cache} = require('hexo-util');
+const cache = new Cache();
 
 // add to postBodyEnd
-hexo.extend.filter.register('theme_inject', function(injects) {
+hexo.extend.filter.register('theme_inject', function(injector) {
   let theme = hexo.theme.config;
-  injects.postBodyEnd.raw('reward.ejs', `
-  <% if (page.reward_settings.enable && !is_index) { %>
-    <%- partial('_partials/post/reward.ejs') -%>
-  <% } %>
-  `, {}, {}, theme.reward_settings.inject_order);
+  injector.register('postBodyEnd', {
+    predicate: ctx => ctx.page.reward_settings.enable && !ctx.is_index,
+    value    : ctx => {
+      let comment = `<div>${theme.reward_settings.comment}</div>`;
+      let box = `<div class="reward-box">${hexo.theme.injector.get('reward').text(ctx)}</div>`;
+      let buttons = Object.keys(theme.reward).map(name => {
+        let options = theme.reward[name];
+        let icon = options.icon ? `<i class="${options.icon}" aria-hidden="true"></i>` : options.name;
+        return `<a class="reward-button ${name}"><span>${icon}</span></a>`;
+      });
+      let group = `<div class="reward-group">${buttons.join('')}</div>`;
+      return `<div class="reward-container">${comment}${box}${group}</div>`;
+    }
+  });
 }, 120);
 
 // add to reward style
-hexo.extend.filter.register('theme_inject', function(injects) {
+hexo.extend.filter.register('theme_inject', function(injector) {
   let rewards = hexo.theme.config.reward;
 
   if (!rewards) {
@@ -23,12 +34,16 @@ hexo.extend.filter.register('theme_inject', function(injects) {
     return;
   }
 
-  Object.keys(rewards).forEach((reward_name) => {
-    let reward_item = rewards[reward_name];
-    let layout = reward_item.layout;
-    injects.reward.file(reward_name, layout || path.join(hexo.theme_dir, 'layout/_partials/post/reward/simple.ejs'), {
-      reward_name,
-      reward_item
-    }, {cache: true});
+  Object.keys(rewards).forEach(name => {
+    let options = rewards[name];
+    let layout = options.layout || path.join(hexo.theme_dir, 'layout/_partials/post/reward/simple.ejs');
+    injector.register('reward', (ctx) => {
+      return cache.apply(name, () => {
+        if (options.image) {
+          options.image = ctx.url_for(options.image);
+        }
+        return hexo.render.renderSync({path: layout}, {name, options});
+      });
+    });
   });
 });
