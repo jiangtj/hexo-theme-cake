06513cf5fd5bec3e1e37f9e20af30e7f9b18c450
MrTT <jiang.taojie@foxmail.com>
2020-03-10 00:26:33 +0800

    ...

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /* global hexo */
 
-const { sep, join } = require('path');
-const Theme = require(join(hexo.plugin_dir, 'hexo/lib/hexo/theme'));
-
-if (hexo.config.theme !== 'hexo-theme-cake') {
+if (hexo.config.theme !== 'cake') {
   return;
 }
+
+const { sep, join } = require('path');
+const Theme = require(join(hexo.plugin_dir, 'hexo/lib/theme'));
+
 hexo.theme_dir = join(hexo.plugin_dir, 'hexo-theme-cake');
 hexo.theme_script_dir = join(hexo.theme_dir, 'scripts') + sep;
 hexo.theme = new Theme(hexo);
