ab8b0954063d8a4f5416f57706147b9a364738df
MrTT <jiang.taojie@foxmail.com>
2020-03-13 02:16:23 +0800

    Add test dependencies

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ if (hexo.config.theme !== 'cake') {
 const { sep, join } = require('path');
 const Theme = require(join(hexo.plugin_dir, 'hexo/lib/theme'));
 
-hexo.theme_dir = join(hexo.plugin_dir, 'hexo-theme-cake');
+hexo.theme_dir = join(hexo.plugin_dir, 'hexo-theme-cake') + sep;
 hexo.theme_script_dir = join(hexo.theme_dir, 'scripts') + sep;
 hexo.theme = new Theme(hexo);
+hexo.theme.ignore = ['**/node_modules/hexo-theme-*/node_modules', '**/node_modules/hexo-theme-*/.git/**'];
 require('./lib/load')(hexo);
---
+++ test/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Hexo = require('hexo');
+// eslint-disable-next-line no-unused-vars
+const hexo = new Hexo(__dirname, { silent: true });
+
+describe('main', () => {
+  //test
+});
