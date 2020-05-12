85dd6d752d11b9b9ecf1b570b74d5b64afca8ef1
Mr.J <116749895@qq.com>
2020-05-12 17:48:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ const Theme = require(join(hexo.plugin_dir, 'hexo/lib/theme'));
 hexo.theme_dir = join(hexo.plugin_dir, 'hexo-theme-cake') + sep;
 hexo.theme_script_dir = join(hexo.theme_dir, 'scripts') + sep;
 hexo.theme = new Theme(hexo);
-hexo.theme.options.ignored = ['**/node_modules/hexo-theme-*/node_modules/**', '**/node_modules/hexo-theme-*/.git/**'].map(item => makeRe(item));
+const ignored = ['**/node_modules/hexo-theme-*/node_modules/**', '**/node_modules/hexo-theme-*/.git/**'];
+hexo.theme.ignore = ignored;
+hexo.theme.options.ignored = ignored.map(item => makeRe(item));
 require('./lib/load')(hexo);
