011146ab3b9b900c49d7f002fd48808b77d01877
jiangtj <116749895@qq.com>
2020-05-12 11:50:21 +0800

    Update box ignored

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ if (hexo.config.theme !== 'cake') {
 }
 
 const { sep, join } = require('path');
+const { makeRe } = require('micromatch');
 const Theme = require(join(hexo.plugin_dir, 'hexo/lib/theme'));
 
 hexo.theme_dir = join(hexo.plugin_dir, 'hexo-theme-cake') + sep;
 hexo.theme_script_dir = join(hexo.theme_dir, 'scripts') + sep;
 hexo.theme = new Theme(hexo);
-hexo.theme.ignore = ['**/node_modules/hexo-theme-*/node_modules', '**/node_modules/hexo-theme-*/.git/**'];
+hexo.theme.options.ignored = ['**/node_modules/hexo-theme-*/node_modules/**', '**/node_modules/hexo-theme-*/.git/**'].map(item => makeRe(item));
 require('./lib/load')(hexo);
