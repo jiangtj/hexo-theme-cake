72bc590e2bf804617528f82d65066b3f6283f9b1
jiangtj <116749895@qq.com>
2020-07-27 17:52:44 +0800

    Update injector2 to v0.2

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ if (hexo.config.theme !== 'cake') {
   return;
 }
 
+const { deepMerge } = require('hexo-util');
+
 const { sep, join } = require('path');
 const { makeRe } = require('micromatch');
 const Theme = require(join(hexo.plugin_dir, 'hexo/lib/theme'));
@@ -15,3 +17,10 @@ const ignored = ['**/node_modules/hexo-theme-*/node_modules/**', '**/node_module
 hexo.theme.ignore = ignored;
 hexo.theme.options.ignored = ignored.map(item => makeRe(item));
 require('./lib/load')(hexo);
+
+// load theme_config before generate
+hexo.extend.filter.register('before_generate', () => {
+  if (hexo.config.theme_config) {
+    hexo.theme.config = deepMerge(hexo.theme.config, hexo.config.theme_config);
+  }
+}, -999);
